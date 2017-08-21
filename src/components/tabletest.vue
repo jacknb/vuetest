469c8d494a13690085327589f7bad386c62038a5
jacknb <zhuningbin@sina.com>
2017-08-21 21:06:42 +0800

    init

---
+++ src/components/tabletest.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        {{message}}
+    </div>
+</template>
+<script>
+export default {
+    name: "tabletest",
+    data() {
+        return {
+            message: "Hello Vue!"
+        }
+    }
+}
+</script>
+
+
